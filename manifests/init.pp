eab22a903790ba2ea3548c1b925a841ba598d90f
jenscobie <jenscobie@users.noreply.github.com>
2016-07-02 23:14:53 -0700

    Ensure correct namewq

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,7 +8,7 @@ class onepassword {
   $host = 'd13itkw33a7sus.cloudfront.net'
   $path = 'dist/1P/mac4/1Password-6.3.1.zip'
 
-  package { '1Password':
+  package { '1Password 6':
     source   => "https://${host}/${path}",
     provider => 'compressed_app',
     flavor => 'zip'
--- spec/classes/onepassword_spec.rb
+++ spec/classes/onepassword_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'onepassword' do
 
   it do
-    should contain_package('1Password').with({
+    should contain_package('1Password 6').with({
       :provider => 'compressed_app',
     })
   end
